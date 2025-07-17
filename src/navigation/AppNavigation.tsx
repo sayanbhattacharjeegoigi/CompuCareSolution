574a90cda3e426f17bdc4b03023944bdc118121d
sayanbhattacharjeegoigi <sayanbhattacharjee@goigi.in>
2025-07-17 19:35:04 +0530

    change stack change

--- src/navigation/AppNavigation.tsx
+++ src/navigation/AppNavigation.tsx
@@ -1,67 +1,44 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import React from "react";
 import Tabs from ".";
 import screens from "../screens";
 import { Routes } from "../utils/Routes";
-
-const Stack = createNativeStackNavigator();
-// Define your stack navigator screens
-const StackNavigator = () => {
-  return (
-    <Stack.Navigator
-      initialRouteName={Routes.LogIn}
-      screenOptions={{
-        headerShown: false,
-      }}
-    >
-      <Stack.Screen name={Routes.LogIn} component={screens.Login} />
-      <Stack.Screen
-        name={Routes.Registration}
-        component={screens.Registration}
-      />
-      <Stack.Screen
-        name={Routes.ServiceRequestDetails}
-        component={screens.ServiceRequestDetails}
-      />
-      <Stack.Screen
-        name={Routes.NotificationDetails}
-        component={screens.NotificationDetails}
-      />
-      <Stack.Screen
-        name={Routes.RepairFlow}
-        component={screens.RepairFlowScreen}
-      />
-      <Stack.Screen
-        name={Routes.ScheduleServiceScreen}
-        component={screens.ScheduleServiceScreen}
-      />
-      <Stack.Screen
-        name={Routes.ScheduleScreen}
-        component={screens.ScheduleScreen}
-      />
-    </Stack.Navigator>
-  );
-};
+const RootStack = createNativeStackNavigator();
 
 const AppNavigator = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName={Routes.Stack}>
-        {/* Stack screens */}
-        <Stack.Screen
-          name={Routes.Tab}
-          component={Tabs}
-          options={{ headerShown: false }}
+      <RootStack.Navigator screenOptions={{ headerShown: false }}>
+        {/* Auth Screens */}
+        <RootStack.Screen name={Routes.LogIn} component={screens.Login} />
+        <RootStack.Screen
+          name={Routes.Registration}
+          component={screens.Registration}
         />
-        <Stack.Screen
-          name={Routes.Stack}
-          component={StackNavigator}
-          options={{ headerShown: false }}
+
+        {/* Main App Screens */}
+        <RootStack.Screen name={Routes.Tab} component={Tabs} />
+        <RootStack.Screen
+          name={Routes.ServiceRequestDetails}
+          component={screens.ServiceRequestDetails}
         />
-      </Stack.Navigator>
+        <RootStack.Screen
+          name={Routes.NotificationDetails}
+          component={screens.NotificationDetails}
+        />
+        <RootStack.Screen
+          name={Routes.RepairFlow}
+          component={screens.RepairFlowScreen}
+        />
+        <RootStack.Screen
+          name={Routes.ScheduleServiceScreen}
+          component={screens.ScheduleServiceScreen}
+        />
+        <RootStack.Screen
+          name={Routes.ScheduleScreen}
+          component={screens.ScheduleScreen}
+        />
+      </RootStack.Navigator>
     </NavigationContainer>
   );
 };
-
-export default AppNavigator;
